c2a5f45ea6082cdc39f2b6e9d0c8d60d79bb6706
Marian Puiu <marian.puiu@tremend.com>
2018-02-02 18:30:16 +0200

    clean target

--- src/test/java/com/selenium/template/tests/TestTheTest.java
+++ src/test/java/com/selenium/template/tests/TestTheTest.java
@@ -2,6 +2,7 @@ package com.selenium.template.tests;
 
 import com.selenium.template.DriverBase;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -13,7 +14,10 @@ public class TestTheTest extends DriverBase {
     @Test(groups = "Test")
     public void Test_01() throws Exception {
 
-        WebDriver driver = getDriver();
+        System.setProperty("webdriver.chrome.driver","C:\\chromedriver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        //WebDriver driver = getDriver();
         driver.get("https://www.google.ro/");
 
         Assert.assertTrue(true);
